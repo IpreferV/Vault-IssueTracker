a4b0a45ef07ef4b2ad6b4d05548cbc6c0c3d82d1
Vault Air <IpreferV@users.noreply.github.com>
2024-12-16 02:48:40 +0800

    Controller Post

---
+++ Vault IssueTracker/Controllers/ReportController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vault_IssueTracker.Model;
+
+namespace Vault_IssueTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly ModelContext _dbContext;
+    public ReportController(ModelContext _dbContext)
+        {
+            _dbContext = _dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Report>>> GetReport()
+        {
+            if (_dbContext.Reports == null)
+            {
+                return NotFound();
+            }
+            return await _dbContext.Reports.ToListAsync();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Report>> GetReport(int id)
+        {
+            if (_dbContext.Reports == null)
+            {
+                return NotFound();
+            }
+            var report = await _dbContext.Reports.FindAsync(id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+            return report;
+        }
+    }
+}
